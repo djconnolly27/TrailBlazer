04f171fd97a46a6dd6c0952056301a683de58dfb
Daniel Connolly <djconnolly27@gmail.com>
2018-03-30 15:12:48 -0400

    Basic outline of functions that will be needed to create this program

---
+++ basic_outline.py
@@ -0,0 +1,33 @@
+'''
+Softdes Final Project: Daniel, Raquel, Lydia
+'''
+
+def get_gps_coordinates():
+    '''
+    Gets a list of coordinates from data uploaded by a user.
+    '''
+    pass
+
+def get_strava_routes():
+    '''
+    Gets a list of lists, where each list contains the coordinates for each point along a route.
+    '''
+    pass
+
+def overlay_route_on_gmaps():
+    '''
+    Takes the lists of route coordinates and overlays the route coordinates on a map.
+    '''
+    pass
+
+def visualize_routes():
+    '''
+    Creates a visualization of the various routes runners/bikers have taken, and/or a visualization of the routes a unique user has taken.
+    '''
+    pass
+
+def suggest_new_route():
+    '''
+    Given a time or distance input by the user, suggests a nonoverlapping or almost entirely nonoverlapping path that starts and ends at the same point and is the path least like the last route taken by the user.
+    '''
+    pass
