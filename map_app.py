0175ffa1925e9411de896a874b6f1a6d0ad7701c
Daniel Connolly <djconnolly27@gmail.com>
2018-04-07 15:52:06 -0400

    Improves web app functionality

--- map_app.py
+++ map_app.py
@@ -9,7 +9,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('my_map.html')
+    return render_template('home.html')
+
+def valid_login(firstname, lastname, email, username, password):
+    if firstname == '':
+        return False
+    if lastname == '':
+        return False
+    if email == '':
+        return False
+    if username == '':
+        return False
+    if password == '':
+        return False
+    return True
+
+def log_the_user_in(firstname, lastname):
+    return render_template('my_map.html', firstname=firstname, lastname=lastname)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['firstname'],
+                        request.form['lastname'],
+                        request.form['email'],
+                        request.form['username'],
+                        request.form['password']):
+            with open('storeformdata.txt', 'w') as f:
+                f.write(request.form['firstname'])
+                f.write(request.form['lastname'])
+                f.write(request.form['email'])
+                f.write(request.form['username'])
+                f.write(request.form['password'])
+            return log_the_user_in(request.form['firstname'], request.form['lastname'])
+        else:
+            error = 'Missing required information'
+    return render_template('error.html', error=error)
+
 
 if __name__ == '__main__':
     app.run()
