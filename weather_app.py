e44cb3892012ab9b94c1132a80341f6a9414c92c
ridunoff <35746189+ridunoff@users.noreply.github.com>
2018-04-13 14:06:32 -0400

    Added weather_app

--- weather_app.py
+++ weather_app.py
@@ -1,29 +1,32 @@
 import plot_a_route as par
 
 
+class weather1:
 
-def get_weather(place):
-    from weather import Weather, Unit
-    weather = Weather(unit=Unit.CELSIUS)
+    def __init__(self, percip=0,date=0,temp=0,high=0,low=0):
+        from weather import Weather, Unit
+        weather = Weather(unit=Unit.CELSIUS)
 
     # Lookup via location name.
 
-    location = weather.lookup_by_location(place)
-    condition = location.condition
-    percip = (condition.text)
-    date = (condition.date)
-    temp = (condition.temp)
+        location = weather.lookup_by_location(place)
+        condition = location.condition
+        self.percip = (condition.text)
+        self.date = (condition.date)
+        self.temp = (condition.temp)
 
     # Get weather forecasts for the upcoming days.
 
-    forecasts = location.forecast
-    forecast = forecasts[1]
+        forecasts = location.forecast
+        forecast = forecasts[1]
 
-    date = (forecast.date)
-    high = (forecast.high)
-    low = (forecast.low)
+
+        self.high = (forecast.high)
+        self.low = (forecast.low)
 
 if __name__ == "__main__":
-    place = input("Enter a place to start: ")
-    place = str(place)
-    weather = get_weather(place)
+    #place = input("Enter a place to start: ")
+    #place = str(place)
+    place = "Olin College"
+    weather = weather1(place)
+    print(weather.date)
