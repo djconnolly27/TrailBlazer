fb8a0101a42f76197d421ae471f945fdc2a56bcb
ridunoff <35746189+ridunoff@users.noreply.github.com>
2018-04-30 22:30:40 -0400

    Colored in plot and cleaned code

---
+++ get_elevations.py
@@ -0,0 +1,39 @@
+'''
+Software Design Final Project: Trail Blazer
+
+get_elevtions.py finds a list of elevations from the longitude and lattitude coordinates using the Google Maps API and plots it using matplotlib.
+'''
+import googlemaps
+from datetime import datetime
+from googlemaps import convert
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+
+GEOCODING_API_KEY = os.environ['GEOCODING_KEY']
+ELEVATION_API_KEY = os.environ['ELEVATION_KEY']
+
+gmaps = googlemaps.Client(key=GEOCODING_API_KEY)
+
+def get_elevation_list():
+    lats = (42.293114, 42.292670, 42.291980, 42.291908, 42.292519,42.292354, 42.293600, 42.295283, 42.296575)
+    longs = (-71.264425, -71.263695, -71.263760, -71.262730, -71.261625,-71.261099,-71.260166,-71.259657, -71.260000)
+    lat_long = []
+    elevations = []
+
+
+    for i in range(len(lats)):
+        lat_long = (lats[i],longs[i])
+        elevationData = gmaps.elevation(lat_long)
+        elevation = elevationData[0]['elevation']
+        elevations.append(elevation)
+    return elevations
+
+def plot_elevation(elevations):
+    plt.plot(elevations,color = 'g')
+    plt.fill_between(np.linspace(0,8,num=9),elevations,0,alpha=0.5, color='g')
+    plt.show()
+
+if __name__ == '__main__':
+    plot_elevation(get_elevation_list())
--- get_start_point.py
+++ get_start_point.py
@@ -6,8 +6,7 @@ get_start_point.py finds a starting location from a user specified point on a go
 import googlemaps
 from datetime import datetime
 from googlemaps import convert
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 import os
 
@@ -15,49 +14,10 @@ GMAP_BASE_URL = "https://www.google.com/maps/embed/v1/place?key="
 
 DIRECTIONS_API_KEY = os.environ["DIRECTIONS_KEY"]
 GEOCODING_API_KEY = os.environ['GEOCODING_KEY']
-ELEVATION_API_KEY = os.environ['ELEVATION_KEY']
+
 
 gmaps = googlemaps.Client(key=GEOCODING_API_KEY)
-
-geocode_result = gmaps.geocode('Olin College of Engineering')
-
-
-LOCATION = "Olin College"
-
-
-def initial_URL(formatted_location):
-    return GMAP_BASE_URL + DIRECTIONS_API_KEY + "&q=" + formatted_location
 
 
 def format_location(location):
     return location.replace(" ","+")
-
-
-if __name__ == '__main__':
-    lats = (42.293114, 42.292670, 42.291980, 42.291908, 42.292519,42.292354, 42.293600, 42.295283, 42.296575)
-    longs = (-71.264425, -71.263695, -71.263760, -71.262730, -71.261625,-71.261099,-71.260166,-71.259657, -71.260000)
-    lat_long = []
-    elevations = []
-
-
-
-    for i in range(len(lats)):
-        lat_long = (lats[i],longs[i])
-        elevationData = gmaps.elevation(lat_long)
-        elevation = elevationData[0]['elevation']
-        elevations.append(elevation)
-
-    plt.plot(elevations)
-    plt.show()
-
-    #print(gmaps.elevation(lat_long)['elevation'])
-
-    """lat_long_list= []
-
-    for i in range(len(lats)):
-        coord_tuple =  [lats[i] , longs[i]]
-        lat_long_list += coord_tuple
-
-    print(lat_long_list)"""
-    #gmaps.elevation(lat_long)
-    #gmaps.elevation_along_path(lat_long_list,10)
