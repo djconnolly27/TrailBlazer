3b80d99f1ec1fdc9bc2ccdf710da5f4866d0eca8
Daniel Connolly <djconnolly27@gmail.com>
2018-05-04 10:51:40 -0400

    Makes code more Pythonic

--- get_elevations.py
+++ get_elevations.py
@@ -1,45 +1,33 @@
 '''
 Software Design Final Project: Trail Blazer
 
-get_elevtions.py finds a list of elevations from the longitude and lattitude coordinates using the Google Maps API and plots it using matplotlib.
+Get_elevations finds the elevations of lat-lng point along a route using the Google Maps API and plots the elevations accordingly.
 '''
-import googlemaps
-from datetime import datetime
-from googlemaps import convert
+from googlemaps import Client as client
 import matplotlib.pyplot as plt
 import numpy as np
-
-import os
 
 GEOCODING_API_KEY = "AIzaSyA4WtBU35L_hml2GTdqtLbHk-IXd76dRuw"
 ELEVATION_API_KEY = "AIzaSyCuHDqWKO7-zWFrU5qhWsaLydNk1mhvaPY"
 
-gmaps = googlemaps.Client(key=GEOCODING_API_KEY) #Creates google map
-
-def get_elevation_list(lats=(42.293114, 42.292670, 42.291980, 42.291908, 42.292519,42.292354, 42.293600, 42.295283, 42.296575),longs=(-71.264425, -71.263695, -71.263760, -71.262730, -71.261625,-71.261099,-71.260166,-71.259657, -71.260000)):
-    """Defines the longitude and lattitude coordinates in lists.
-    Fills elevations with a list of corresponding elevations.
+def get_elevation_list(lats, longs):
+    """Defines the longitude and lattitude coordinates in lists. Fills elevations with a list of corresponding elevations.
     """
-    lats = lats
-    longs = longs
     elevations = []
+
+    map_client = client(key=GEOCODING_API_KEY)
 
     #Populate elevations
     for i in range(len(lats)):
-        lat_long = (lats[i],longs[i]) #Format lattitute and longitude
-        elevationData = gmaps.elevation(lat_long) #Get the JSON files from the Google Maps API
-        elevation = elevationData[0]['elevation'] #Extract the elevation
+        lat_long = (lats[i], longs[i]) #Format lattitute and longitude
+        elevation_data = map_client.elevation(lat_long) #Get the JSON files from the Google Maps API
+        elevation = elevation_data[0]['elevation'] #Extract the elevation
         elevations.append(elevation) #add the elevation to the list
     return elevations
 
-def plot_elevation(elevations,d=8):
-    d = d*1000 #convert from km to m
-    distances = np.linspace(0,d,num=len(elevations)) #create array for plotting
-    plt.plot(distances,elevations,color = 'g')
-    plt.fill_between(distances,elevations,0,alpha=0.5, color='g')
-    plt.xlabel('Distance (m)')
-    plt.ylabel('Elevation (m)')
+def plot_elevation(elevations, distance):
+    ''' Plots the elevations of points along a route over the course of the route. '''
+    x_axis = np.linspace(0, distance, num=len(elevations))
+    plt.plot(x_axis, elevations, color='g')
+    plt.fill_between(x_axis, elevations, 0, alpha=0.5, color='g')
     plt.show()
-
-if __name__ == '__main__':
-    plot_elevation(get_elevation_list())
